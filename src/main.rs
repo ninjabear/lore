c241e02038889ce2767b570a1234b9fe95e06950
Ed Lewis <edward.richard.lewis@gmail.com>
2015-02-14 02:39:28 +0000

    use nickel.rs demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate nickel;
+
+use std::io::net::ip::Ipv4Addr;
+use nickel::{ Nickel, Request, Response, HttpRouter };
+
 fn main() {
-    println!("Hello, world!");
+  let mut server = Nickel::new();
+
+  fn a_handler (_request: &Request, response: &mut Response) {
+    response.send("hello world");
+  }
+
+  server.get("/hello", a_handler);
+  server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
